68f91b8358df299d6b803c6813e76e11f3a07e97
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-05-24 10:15:32 +0200

    migrate(4.x): rewrite highestrating widget without deprecated annotation_calculation API
    - elgg_get_entities_from_annotation_calculation() deprecated in 3.x, removed in 5.x
    - replace with elgg_get_entities() + QueryBuilder closure on joinAnnotationTable()
    - ordered by AVG(value) DESC via OrderByClause closure
    - preserves load-bearing 'starrating' annotation behaviour

--- views/default/widgets/highestrating/content.php
+++ views/default/widgets/highestrating/content.php
@@ -1,14 +1,15 @@
 <?php
+
+use Elgg\Database\Clauses\OrderByClause;
+use Elgg\Database\QueryBuilder;
+
 $widget = elgg_extract('entity', $vars);
+$page_owner = elgg_get_page_owner_entity();
 
-if (elgg_in_context('profile') || elgg_in_context('groups')) {
-	$page_owner = elgg_get_page_owner_entity();
-}
-
-if ($page_owner instanceof \ElggUser) {
+if (elgg_in_context('profile') && $page_owner instanceof \ElggUser) {
 	$owner_guid = $page_owner->guid;
 	$container_guid = ELGG_ENTITIES_ANY_VALUE;
-} else if ($page_owner instanceof \ElggGroup) {
+} else if (elgg_in_context('groups') && $page_owner instanceof \ElggGroup) {
 	$owner_guid = ELGG_ENTITIES_ANY_VALUE;
 	$container_guid = $page_owner->guid;
 } else {
@@ -17,30 +18,47 @@ if ($page_owner instanceof \ElggUser) {
 }
 
 $annotation_names = elgg_stars_get_rating_annotation_names();
-$entity_rating = [
+$num_display = (int) ($widget->num_display ?: 10);
+
+// Replacement for elgg_get_entities_from_annotation_calculation() (deprecated in 3.x, removed in 5.x).
+// Joins the annotations table for the configured rating annotation names and
+// orders by AVG(value) DESC via a QueryBuilder closure.
+$entities = elgg_get_entities([
 	'types' => $widget->content_type,
 	'owner_guid' => $owner_guid,
 	'container_guid' => $container_guid,
-	'annotation_names' => $annotation_names,
-	'calculation' => 'avg',
-	'order_by' => 'annotation_calculation desc',
-	'limit' => $widget->num_display
-];
-
-$entities = elgg_get_entities_from_annotation_calculation($entity_rating);
+	'limit' => $num_display,
+	'preload_owners' => true,
+	'annotation_name_value_pairs' => [
+		[
+			'name' => $annotation_names,
+		],
+	],
+	'order_by' => [
+		new OrderByClause(
+			function (QueryBuilder $qb) use ($annotation_names) {
+				$alias = $qb->joinAnnotationTable('e', 'guid', $annotation_names, 'inner', 'star_rating');
+				return "AVG({$alias}.value)";
+			},
+			'desc'
+		),
+	],
+	'group_by' => 'e.guid',
+]);
 
 elgg_push_context('starrating');
+$list = '';
 
 foreach ($entities as $entity) {
 	$title = elgg_view('output/url', [
 		'text' => (isset($entity->title)) ? $entity->title : $entity->name,
 		'href' => $entity->getURL(),
-		'is_trusted' => true
+		'is_trusted' => true,
 	]);
 
 	if (!empty($entity->description)) {
 		$desc = elgg_view('output/longtext', [
-			'value' => elgg_get_excerpt($entity->description)
+			'value' => elgg_get_excerpt($entity->description),
 		]);
 	} else {
 		$desc = '';
@@ -50,12 +68,12 @@ foreach ($entities as $entity) {
 	$entity_rating = elgg_stars_get_entity_rating_values($entity, $annotation_names);
 
 	$rating = elgg_view('output/stars', [
-		'value' => $entity_rating['value']
+		'value' => $entity_rating['value'],
 	]);
 	$rating .= '<div class="elgg-stars-rating-caption">' . elgg_echo('stars:stats', [$entity_rating['value'], $entity_rating['max'], $entity_rating['count']]) . '</div>';
 
 	$list .= '<li>' . elgg_view_image_block($icon, $title . $desc, [
-		'image_alt' => $rating
+		'image_alt' => $rating,
 	]) . '</li>';
 }
 
